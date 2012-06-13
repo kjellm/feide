4b457fd96a90599dc4abcaa333696c1b7cc72319
Kjell-Magne Øierud <kjellm@acm.org>
2012-06-13 20:54:42 +0200

    Stuff

--- examples/sinatra.rb
+++ examples/sinatra.rb
@@ -2,14 +2,35 @@ require 'sinatra'
 require 'saml'
 require 'feide_sp'
 
-set :port, 80 # Protip: Use rvmsudo to start this app if you use rvm
+##### CONFIGURATION
+#
 
-xml = REXML::Document.new(open('feide.xml', 'r')).root
+# This need to be set to the same port as the one feide expects to
+# connect to.
+# 
+# (If this happens to be a privileged port and you use RVM, use
+# rvmsudo to start the app.)
+set :port, 80 
 
-use FeideSP, { :meta => xml }
+# Location of the SAML Metadata document containing an
+# <EntitiesDescriptor> as the root element with both your and feides
+# <EntityDescriptor>s.
+metadata = 'feide.xml'
+
+#
+##### END: CONFIGURATION
+
+meta = SAML::Metadata::Document.new(open(metadata, 'r')).root
+
+use FeideSP, { :meta => meta }
 
 get '/' do
-  'Hei'
+  <<EOT
+<h1>The Sinatra Example</h1>
+<ul>
+  <li><a href="/feide/signon/">Signon</a>
+  <li><a href="/feide/logout/">Logout</a>
+EOT
 end
 
 
