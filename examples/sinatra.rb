375900cb4e3eea994c86e665e5d1d17b9d886682
Kjell-Magne Øierud <kjellm@oierud.net>
2012-06-28 20:20:49 +0200

    Sintra example: store user info in session, use session data in logout URL

--- examples/sinatra.rb
+++ examples/sinatra.rb
@@ -20,6 +20,8 @@ metadata = 'feide.xml'
 #
 ##### END: CONFIGURATION
 
+enable :sessions
+
 meta = SAML::Metadata::EntitiesDescriptor.from_xml(SAML::Metadata::Document.new(open(metadata, 'r')).root)
 
 use Feide::RackServiceProvider, { :meta => meta }
@@ -29,18 +31,22 @@ get '/' do
 <h1>The Sinatra Example</h1>
 <ul>
   <li><a href="/feide/signon?relay_state=foo">Signon</a>
-  <li><a href="/feide/logout?user=test@feide.no&relay_state=bar">Logout</a>
+  <li><a href="/feide/logout?user=#{session[:feide_subject_id]}&session_index=#{session[:feide_session_index]}&relay_state=bar">Logout</a>
 EOT
 end
 
 post meta.sp_assertion_consumer_service.location.path do
   r = env['X-SAMLResponse'].core_response
   rs = env['X-SAMLResponse'].relay_state
+  an = r.assertions.first.authn_statements.first
+
+  session[:feide_subject_id] = r.assertions.first.subject.name_id
+  session[:feide_session_index] = an.session_index
+
   str = "<pre>Status success?: #{r.success?}\n"
   str << "RelayState: #{rs}\n"
   str << "Subject:    #{r.assertions.first.subject.name_id}\n"
   str << "AuthnStatement:\n"
-  an = r.assertions.first.authn_statements.first
   str << "         AuthnInstant: #{an.authn_instant}\n"
   str << "  SessionNotOnOrAfter: #{an.session_not_on_or_after}\n"
   str << "         SessionIndex: #{an.session_index}\n"
@@ -48,6 +54,7 @@ post meta.sp_assertion_consumer_service.location.path do
   r.assertions.first.attribute_statement.attributes.each do |a|
     str << "  #{a.name} #{a.attribute_values}\n"
   end
+  str << "</pre><a href=\"/\">Home</a>"
   str
 end
   
@@ -56,5 +63,6 @@ get meta.sp_single_logout_service.location.path do
   rs = env['X-SAMLResponse'].relay_state
   str = "<pre>Status success?: #{r.success?}\n"
   str << "RelayState: #{rs}\n"
+  str << "</pre><a href=\"/\">Home</a>"
   str
 end
