882c9868f5742cc28b545d6135cadb2133f8adc7
Kjell-Magne Øierud <kjellm@oierud.net>
2012-06-29 10:43:32 +0200

    Add some API doc

--- lib/feide/rack_service_provider.rb
+++ lib/feide/rack_service_provider.rb
@@ -2,20 +2,34 @@ require 'saml'
 require 'rack'
 
 module Feide
-  class RackServiceProvider
 
+  # Rack middleware for integrating with the FEIDE Identity
+  # Provider. Handles single sign on and single logout.
+  # 
+  # This middleware hijacks signon_path and logout_path
+  # 
+  # == opts
+  # :meta:: SAML::Metadata::EntitiesDescriptor
+  # :signon_path:: Default is '/feide/signon'
+  # :logout_path:: Default is '/feide/logout'
+  #
+  class RackServiceProvider
+    
     def initialize(app, opts)
       @meta = opts[:meta]
       @app  = app
 
       handler_class = opts[:handler_class] || SAMLHandler
       @handler_obj = handler_class.new(@meta)
+
+      signon_path = opts[:signon_path] || '/feide/signon'
+      logout_path = opts[:logout_path] || '/feide/logout'
       
       @dispatch = {
         'GET' => {
-          '/feide/signon' => :signon,
-          '/feide/logout' => :logout,
-            @meta.sp_single_logout_service.location.path => :consume_logout,
+          signon_path => :signon,
+          logout_path => :logout,
+          @meta.sp_single_logout_service.location.path => :consume_logout,
         },
         'POST' => {
           @meta.sp_assertion_consumer_service.location.path => :consume,
